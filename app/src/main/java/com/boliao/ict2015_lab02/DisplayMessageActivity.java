2e9c06dfbaf06eb9dd3cef537ba8089379a9fce9
chektien <chek@gamesstudio.org>
2017-01-09 15:27:58 +0800

    added lifecycle counters

--- app/src/main/java/com/boliao/ict2015_lab02/DisplayMessageActivity.java
+++ app/src/main/java/com/boliao/ict2015_lab02/DisplayMessageActivity.java
@@ -3,14 +3,24 @@ package com.boliao.ict2015_lab02;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+    public static final String TAG = "DisplayMessageActivity";
+    private int onCreateCount = 0;
+    private int onRestartCount = 0;
+    private int onStartCount = 0;
+    private int onResumeCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "Entered onCreate() " + ++onCreateCount + " times.");
+
         setContentView(R.layout.activity_display_message);
 
         Intent intent = getIntent();
@@ -21,5 +31,28 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
         layout.addView(textView);
+
+        // close and go back to main activity
+        //final Intent intent2 = new Intent(this, MainActivity.class);
+        Button buttonClose = (Button)  findViewById(R.id.button_close);
+        buttonClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "Entered onStart() " + ++onStartCount + " times.");
+    }
+    public void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "Entered onRestart() " + ++onRestartCount + " times.");
+    }
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG, "Entered onResume() " + ++onResumeCount + " times.");
     }
 }
--- app/src/main/java/com/boliao/ict2015_lab02/MainActivity.java
+++ app/src/main/java/com/boliao/ict2015_lab02/MainActivity.java
@@ -3,16 +3,26 @@ package com.boliao.ict2015_lab02;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String TAG = "MainActivity";
     public final static String EXTRA_MESSAGE = "com.boliao.ictlab.EXTRA";
+
+    // debug counters
+    private int onCreateCount = 0;
+    private int onRestartCount = 0;
+    private int onStartCount = 0;
+    private int onResumeCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "Entered onCreate() " + ++onCreateCount + " times.");
     }
 
     public void sendMessage(View view) {
@@ -21,5 +31,18 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "Entered onStart() " + ++onStartCount + " times.");
+    }
+    public void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "Entered onRestart() " + ++onRestartCount + " times.");
+    }
+    public void onResume() {
+        super.onResume();
+        Log.i(TAG, "Entered onResume() " + ++onResumeCount + " times.");
     }
 }
